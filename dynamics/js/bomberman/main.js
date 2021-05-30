8e9827adbd583e8af4645e0d1f440ba4d7cc100c
Francisco-Galindo <paqui10718@gmail.com>
2021-05-30 16:32:56 -0500

    FIX: Modificando tipo de indentación

--- dynamics/js/bomberman/main.js
+++ dynamics/js/bomberman/main.js
@@ -10,69 +10,69 @@ let startTime;
 /* Se modifica el tamaño del canvas para que sea el mismo que su
 div padre */
 function escalarCanvas() {
-    canvas.height = gameDiv.clientHeight;
-    canvas.width = gameDiv.clientWidth;
+	canvas.height = gameDiv.clientHeight;
+	canvas.width = gameDiv.clientWidth;
 }
 
 // Dibuja todos los elementos del juego (personajes, bloques, etc.)
 function draw(factor) {
-    
-    player.draw(ctx, factor);
+
+	player.draw(ctx, factor);
 }
 
 
 function drawPauseScreen(factor) {
-    ctx.beginPath();
-    ctx.globalAlpha = 0.3;
-    ctx.fillRect(0,0, canvas.width, canvas.height);
-    ctx.globalAlpha = 1.0;
-    ctx.font = "50px Consolas";
-    ctx.fillText(`Pausa`, 0, 20 * factor);
-    ctx.fill()
-    ctx.closePath();
+	ctx.beginPath();
+	ctx.globalAlpha = 0.3;
+	ctx.fillRect(0,0, canvas.width, canvas.height);
+	ctx.globalAlpha = 1.0;
+	ctx.font = "50px Consolas";
+	ctx.fillText(`Pausa`, 0, 20 * factor);
+	ctx.fill()
+	ctx.closePath();
 }
 
 /* Esta es la función que desencadena el resto de funciones que se deben 
 realizar cada vez que se va a dibujar un nuevo cuadro */
 function gameCycle() {
-    escalarCanvas();
-    let factor = canvas.height / VIRTUALHEIGHT;
+	escalarCanvas();
+	let factor = canvas.height / VIRTUALHEIGHT;
 
-    draw(factor);
+	draw(factor);
 
-    if (!pause) {
-    } else {
-        drawPauseScreen(factor);
-    }
+	if (!pause) {
+	} else {
+		drawPauseScreen(factor);
+	}
 
-    requestAnimationFrame(gameCycle);
+	requestAnimationFrame(gameCycle);
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    gameDiv = document.getElementById("juego");
-    canvas = document.getElementById("juego-canvas");
-    ctx = canvas.getContext("2d");
-    console.log(canvas)
+	gameDiv = document.getElementById("juego");
+	canvas = document.getElementById("juego-canvas");
+	ctx = canvas.getContext("2d");
+	console.log(canvas)
 
 
-    gameDiv.addEventListener("focus", () => {
-        gameFocused = true;
-    })
+	gameDiv.addEventListener("focus", () => {
+		gameFocused = true;
+	})
 
-    gameDiv.addEventListener("blur", () => {
-        gameFocused = false;
-        pause = true;
-    })
+	gameDiv.addEventListener("blur", () => {
+		gameFocused = false;
+		pause = true;
+	})
 
-    gameDiv.addEventListener("keydown", (event) => {
+	gameDiv.addEventListener("keydown", (event) => {
 
-        if (event.key === "Escape") {
-            pause = !pause
-        }
-    })
+		if (event.key === "Escape") {
+			pause = !pause
+		}
+	})
 
-    player = new Pulpito(0,0,1,1,null)
-    gameCycle()
+	player = new Pulpito(0,0,1,1,null)
+	gameCycle()
 })
--- dynamics/js/bomberman/util.js
+++ dynamics/js/bomberman/util.js
@@ -3,36 +3,43 @@ const VIRTUALWIDTH = 256;
 const TILESIZE = 16;
 const SPRITESIZE = 16;
 
+let map = Array.from(Array((VIRTUALHEIGHT / TILESIZE) -2), () => 
+    Array.from(Array((VIRTUALWIDTH / TILESIZE)), () => null));
+
+map[0][0] = "lel"
+map[1][0] = "lel"
+
+
 class Entidad {
-    constructor(spriteSheet, xPos, yPos, tileSpanX, tileSpanY) {
-        this.spriteSheet = spriteSheet;
-        this.spritePos =  [0,0]
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.tileSpanX = tileSpanX;
-        this.tileSpanY = tileSpanY;
-    }
+	constructor(spriteSheet, xPos, yPos, tileSpanX, tileSpanY) {
+		this.spriteSheet = spriteSheet;
+		this.spritePos =  [0,0]
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.tileSpanX = tileSpanX;
+		this.tileSpanY = tileSpanY;
+	}
 
-    draw(ctx, scalingFactor) {
-        // Evitar que se aplique Anti-Aliasing, que volvería la imagen borrosa
-        ctx.imageSmoothingEnabled = false;
+	draw(ctx, scalingFactor) {
+		// Evitar que se aplique Anti-Aliasing, que volvería la imagen borrosa
+		ctx.imageSmoothingEnabled = false;
 
 
-        ctx.drawImage(this.spriteSheet, SPRITESIZE * this.spritePos[0], SPRITESIZE * this.spritePos[1], SPRITESIZE, SPRITESIZE, this.xPos * scalingFactor, this.yPos * scalingFactor, (this.tileSpanX * TILESIZE) * scalingFactor, (this.tileSpanY * TILESIZE) * scalingFactor)
-    }
+		ctx.drawImage(this.spriteSheet, SPRITESIZE * this.spritePos[0], SPRITESIZE * this.spritePos[1], SPRITESIZE, SPRITESIZE, this.xPos * scalingFactor, this.yPos * scalingFactor, (this.tileSpanX * TILESIZE) * scalingFactor, (this.tileSpanY * TILESIZE) * scalingFactor)
+	}
 }
 
 class Pulpito extends Entidad {
-    constructor(xPos, yPos, lives, maxBombsInBag, powerUp) {
-        super(null, xPos, yPos, 1, 1)
+	constructor(xPos, yPos, lives, maxBombsInBag, powerUp) {
+		super(null, xPos, yPos, 1, 1)
 
-        let spriteSheet = new Image();
-        spriteSheet.src = "../statics/img/bomberman/sprites/pulpito_sprite_sheet_lq.png";
-        this.spriteSheet = spriteSheet
-        this.lives = lives;
-        this.score = 0;
-        this.maxBombsInBag = maxBombsInBag;
-        this.powerUp = powerUp;
-        this.powerUpTimer = 0;
-    }
+		let spriteSheet = new Image();
+		spriteSheet.src = "../statics/img/bomberman/sprites/pulpito_sprite_sheet_lq.png";
+		this.spriteSheet = spriteSheet
+		this.lives = lives;
+		this.score = 0;
+		this.maxBombsInBag = maxBombsInBag;
+		this.powerUp = powerUp;
+		this.powerUpTimer = 0;
+	}
 }
